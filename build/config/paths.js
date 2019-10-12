c6f2248aaf687b37c8de73949a66e120f4f54bc7
Filipe Graça <filipegraca1992@gmail.com>
2019-10-12 15:36:57 +0100

    Initial setup for coachee website

--- build/config/paths.js
+++ build/config/paths.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const path = require('path');
 const fs = require('fs');
@@ -69,9 +69,9 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('build'),
-  appPublic: resolveApp('public'),
-  appHtml: resolveApp('public/index.html'),
+  appBuild: resolveApp('build/build'),
+  appPublic: resolveApp('build/public'),
+  appHtml: resolveApp('build/public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),

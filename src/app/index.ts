fdbcb0592b658b7d1a5997e90235478d1b42836d
Filipe Costa <filipe.costa@perkbox.com>
2019-11-01 10:57:51 +0000

    removed create react app

--- src/app/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * This is where we will boostrap our application
- * Setup the Providers if needed, i.e. Redux
- * Setup scripts for Purechat, Facebook/Google single sign on JS sdk
- */
--- src/components/Layout/Footer.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import {Container} from "react-bootstrap"
-
-const Footer = (props: any) => <Container {...props} />
-
-export default Footer

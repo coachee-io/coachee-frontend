1ec924b7ede349f8315b8d5d25ce53ba1f14593a
Filipe Graça <filipegraca1992@gmail.com>
2019-11-02 16:01:31 +0000

    file structure, svgs and global css file

---
+++ src/ui/colors/index.ts
@@ -0,0 +1,6 @@
+export default {
+  white: "#FFFFFF",
+  black: "#000000",
+  primary: "#7379E2",
+  accent: "#C9E265"
+}
---
+++ src/ui/fonts/index.ts
@@ -0,0 +1,23 @@
+import {css} from "styled-components"
+
+const LINE_HEIGHT = 1.5
+
+export const Lato = css`
+  font-family: Lato, sans-serif;
+  line-height: ${LINE_HEIGHT};
+`
+
+export const OpenSans = css`
+  font-family: Lato, sans-serif;
+  line-height: ${LINE_HEIGHT};
+`
+
+export const LatoBold = css`
+  font-weight: bold;
+  ${Lato};
+`
+
+export const OpenSansBold = css`
+  font-weight: bold;
+  ${OpenSans};
+`

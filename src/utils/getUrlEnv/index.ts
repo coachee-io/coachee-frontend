0eeb7824237b7bb01bd8a3309963a9c1dcb6ce80
Filipe Costa <filipe.costa@perkbox.com>
2020-03-11 22:04:20 +0000

    netlify toml file

--- src/utils/getUrlEnv/index.ts
+++ src/utils/getUrlEnv/index.ts
@@ -1,4 +1,17 @@
 /**
  * getUrlForEnvironment for either production or staging
  */
-export const getUrlEnvironmentDomain = (): string | undefined => process.env.REACT_APP_API_URL
+export const getUrlEnvironmentDomain = (): string | undefined => {
+  // Production env
+  if (process.env.REACT_APP_CONTEXT_ENV === 'production') {
+    return process.env.REACT_APP_PRODUCTION_API
+  }
+
+  // Staging env
+  if (process.env.REACT_APP_CONTEXT_ENV === 'staging') {
+    return process.env.REACT_APP_STAGING_API
+  }
+
+  // Default to local environment
+  return process.env.REACT_APP_LOCAL_API
+}

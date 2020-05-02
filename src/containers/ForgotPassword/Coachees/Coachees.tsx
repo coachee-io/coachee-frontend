1bbc3f6dd158039b055dbe681122f387b39808f6
Filipe Costa <filipegraca1992@gmail.com>
2020-05-02 19:42:48 +0100

    loading status

--- src/containers/ForgotPassword/Coachees/Coachees.tsx
+++ src/containers/ForgotPassword/Coachees/Coachees.tsx
@@ -23,12 +23,13 @@ class CoacheesForgotPassword extends PureComponent<Props, State> {
 
   onSubmit = (values: {email: string}) => {
     const {email} = values
+    this.setState({isLoading: true})
     PlatformService.coacheeForgotPassword(email)
       .then(() => {
-        this.setState({isSuccessful: true})
+        this.setState({isSuccessful: true, isLoading: false})
       })
       .catch((error) => {
-        this.setState({error})
+        this.setState({error, isLoading: false})
       })
   }
 

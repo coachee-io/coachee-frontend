850551e7a6365ae284215e050ac6107a0983acc1
Filipe Costa <filipegraca1992@gmail.com>
2020-07-15 22:33:43 +0100

    hr spacing

--- src/components/Hr/index.tsx
+++ src/components/Hr/index.tsx
@@ -4,7 +4,7 @@ const Hr = styled.hr`
   width: 90%;
   margin: 0 auto;
   border: 0.5px solid rgba(0, 0, 0, 0.2);
-  margin: 16px 0;
+  margin: 16px 0 32px;
   &:last-child {
     display: none;
   }

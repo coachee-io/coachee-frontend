8e1cc71702f2d4ec4f726b314fc17dce59d3c414
Filipe Costa <filipegraca1992@gmail.com>
2020-07-15 23:18:32 +0100

    take the whole content width instead

--- src/components/Hr/index.tsx
+++ src/components/Hr/index.tsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
 const Hr = styled.hr`
-  width: 80%;
+  width: 100%;
   border: 0.5px solid rgba(0, 0, 0, 0.2);
-  margin: 16px auto 32px auto;
+  margin: 16px 0px 32px 0px;
   &:last-child {
     display: none;
   }

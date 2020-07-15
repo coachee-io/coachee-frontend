16872a01967076749b3a9d155c492f1f41f29372
Filipe Costa <filipegraca1992@gmail.com>
2020-07-15 23:21:24 +0100

    rgb

--- src/components/Hr/index.tsx
+++ src/components/Hr/index.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const Hr = styled.hr`
   width: 100%;
-  border: 0.5px solid rgba(0, 0, 0, 0.2);
+  border: 0.5px solid rgba(159, 159, 159, 0.2);
   margin: 16px 0px 32px 0px;
   &:last-child {
     display: none;
